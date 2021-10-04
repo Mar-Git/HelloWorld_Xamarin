a64ea8402475bdc57294af8bf819d9c9fa2fe094
Mar <mar.accri@gmail.com>
2021-10-04 13:12:30 +0200

    Hola mundo mas la clase persona en xamarin

--- HelloWorld_Xamarin/MainPage.xaml.cs
+++ HelloWorld_Xamarin/MainPage.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Entidades;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+
 
 namespace HelloWorld_Xamarin
 {
@@ -13,6 +15,15 @@ namespace HelloWorld_Xamarin
         public MainPage()
         {
             InitializeComponent();
+        }
+        /**
+         * 
+         */
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            Entry entry = txtNombre;//nueva entrada que coja el txt que escriba el usuario txtNombre esta declarado en MainPage.xaml
+            clsPersona persona1 = new clsPersona(entry.Text);
+            await DisplayAlert ("Mensaje", "Hola "+persona1.Nombre+" saludos", "OK");
         }
     }
 }
